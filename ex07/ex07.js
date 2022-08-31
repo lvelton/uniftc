14e3503a1de295aeceef98369b1bfd03b212a60a
Elivelton Cerqueira <elivelton.cerq@gmail.com>
2022-08-31 17:06:45 -0300

    Subindo as últimas questões resolvidas em sala

---
+++ ex07/ex07.js
@@ -0,0 +1,8 @@
+var xA = Number(window.prompt("Digite o xA"))
+var xB = Number(window.prompt("Digite o xB"))
+var yA = Number(window.prompt("Digite o yA"))
+var yB = Number(window.prompt("Digite o yB"))
+
+var d = Math.sqrt((xB - xA)**2 + (yB - yA)**2)
+
+document.write("A distância é:"+d)
