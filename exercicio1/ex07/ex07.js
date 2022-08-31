c98b74a228b567686d7fd88b0c7c0ada0f68e798
Elivelton Cerqueira <elivelton.cerq@gmail.com>
2022-08-31 17:08:48 -0300

    Subindo as últimas questões resolvidas em sala

--- exercicio1/ex07/ex07.js
+++ exercicio1/ex07/ex07.js
@@ -1,6 +1,6 @@
 var xA = Number(window.prompt("Digite o xA"))
-var xB = Number(window.prompt("Digite o xB"))
 var yA = Number(window.prompt("Digite o yA"))
+var xB = Number(window.prompt("Digite o xB"))
 var yB = Number(window.prompt("Digite o yB"))
 
 var d = Math.sqrt((xB - xA)**2 + (yB - yA)**2)
